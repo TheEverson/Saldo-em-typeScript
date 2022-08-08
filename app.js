26dfbf15dcbe21c41c159a1e567f0e54eff1bf00
TheEverson <eversonaguiar2000@gmail.com>
2022-08-08 20:59:57 -0300

    Conclusão do beta do depositador

---
+++ app.js
@@ -0,0 +1,31 @@
+// O código abaixo tem alguns erros e não funciona como deveria. Você pode identificar quais são e corrigi-los em um arquivo TS?
+
+let botaoAtualizar = document.getElementById('atualizar-saldo');
+let botaoLimpar = document.getElementById('limpar-saldo');
+let soma = document.getElementById('soma');
+let campoSaldo = document.getElementById('campo-saldo');
+
+campoSaldo.innerHTML = 0
+
+function somarAoSaldo(ssoma) {
+    campoSaldo.innerHTML += Number(ssoma)
+}
+
+function limparSaldo() {
+    campoSaldo.innerHTML = '';
+}
+
+botaoAtualizar.addEventListener('click', () => {
+    somarAoSaldo(soma.value);
+});
+
+botaoLimpar.addEventListener('click', () => {
+    limparSaldo();
+});
+
+/**
+    <h4>Valor a ser adicionado: <input id="soma"> </h4>
+    <button id="atualizar-saldo">Atualizar saldo</button>
+    <button id="limpar-saldo">Limpar seu saldo</button>
+    <h1>"Seu saldo é: " <span id="campo-saldo"></span></h1>
+ */
